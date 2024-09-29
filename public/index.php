50efdbc9686f1d7815b8938b3b85bba15602ff56
Avendciory <avendciory@gmail.com>
2024-09-29 15:27:32 +0300

    Created Database class and established connection with DB

--- public/index.php
+++ public/index.php
@@ -1,14 +1,13 @@
 <?php
-
 require '../helpers.php';
-
+require basePath('Database.php');
 require basePath('Router.php');
-
+// Instantiate the router.
 $router = new Router();
-
+// Get routes
 require basePath('routes.php');
-
+// Get URI and REQUEST METHOD
 $uri = $_SERVER['REQUEST_URI'];
 $method = $_SERVER['REQUEST_METHOD'];
-
+// Route to specific controller
 $router->route($uri, $method);
