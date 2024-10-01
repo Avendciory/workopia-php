d117edb76c72479081cdc3bc8af87d1d6baeec18
Avendciory <Avendciory@gmail.com>
2024-10-01 14:33:33 +0300

    Implemented form validation and form submission route

--- App/controllers/ListingController.php
+++ App/controllers/ListingController.php
@@ -63,4 +63,39 @@ class ListingController
             'listing' => $listing
         ]);
     }
+
+    /**
+     * Store form data to the databse
+     * 
+     * @return void
+     */
+    public function store()
+    {
+        $allowedFields = ['title', 'description', 'salary', 'tags', 'company', 'address', 'city', 'state', 'phone', 'email', 'requirements', 'benefits'];
+
+        $newListingData = array_intersect_key($_POST, array_flip($allowedFields));
+
+        $newListingData['user_id'] = 1;
+
+        $newListingData = array_map('sanitize', $newListingData);
+
+        $requiredFields = ['title', 'description', 'email', 'city', 'state'];
+
+        $errors = [];
+
+        foreach ($requiredFields as $field) {
+            if (empty($newListingData[$field]) || !Validation::string($newListingData[$field])) {
+                $errors[$field] = ucfirst($field) . ' is required...';
+            }
+        }
+
+        if (!empty($errors)) {
+            loadView('listings/create', [
+                'errors' => $errors,
+                'listing' => $newListingData
+            ]);
+        } else {
+            echo 'Success...';
+        }
+    }
 }
