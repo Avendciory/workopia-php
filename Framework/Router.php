37ebf63ee96eb73f94f06016d811e2f898b93e42
Avendciory <Avendciory@gmail.com>
2024-10-02 10:35:10 +0300

    Implemented deletion of job listing

--- Framework/Router.php
+++ Framework/Router.php
@@ -84,6 +84,11 @@ class Router
     public function route($uri)
     {
         $requestMethod = $_SERVER['REQUEST_METHOD'];
+
+        //Check for _method input
+        if ($requestMethod === 'POST' && isset($_POST['_method'])) {
+            $requestMethod = strtoupper($_POST['_method']);
+        }
         //Split URI into segments
         $uriSegments = explode('/', trim($uri, '/'));
 
